2d06da7a94928f362ab2417818a6ede4be139168
SavandiNarmada <savandi.narmada@gmail.com>
2024-02-28 20:50:35 +0530

    Creates files.

---
+++ navigation/appNavigation.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function appNavigation() {
+  return (
+    <View>
+      <Text>appNavigation</Text>
+    </View>
+  )
+}
