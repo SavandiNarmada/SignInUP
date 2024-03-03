405a3662910db632f9d393481c12138b4c953033
SavandiNarmada <savandi.narmada@gmail.com>
2024-03-03 10:57:30 +0530

    Finished the WelcomeScreen.js

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 
 export default function WelcomeScreen() {
+  const navigation = useNavigation();
   return (
     <SafeAreaView className="flex-1" style={{backgroundColor: themeColors.bg}}>
       <View className="flex-1 flex justify-around my-4">
@@ -15,7 +16,7 @@ export default function WelcomeScreen() {
         </View>
 
         <View className="space-y-4">
-          <TouchableOpacity className="py-3 bg-yellow-400 mx-7 rounded-xl">
+          <TouchableOpacity  onPress={()=>navigation.navigate('SignUp')}    className="py-3 bg-yellow-400 mx-7 rounded-xl">
             <Text className="text-xl font-bold text-center text-gray-700">
                   SignUp
             </Text>
@@ -23,7 +24,7 @@ export default function WelcomeScreen() {
 
           <View className="flex-row justify-center">
             <Text className="text-white font-semibold">Already have an account?</Text>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={()=> navigation.navigate('Login')}>
               <Text className="font-semibold text-yellow-400">Log In</Text>
             </TouchableOpacity>
           </View>
