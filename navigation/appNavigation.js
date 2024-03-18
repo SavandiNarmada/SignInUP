53e167d8e9869d3a9362ef9380f9bf0f1cbcadb3
SavandiNarmada <savandi.narmada@gmail.com>
2024-03-18 16:02:27 +0530

    delete

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import WelcomeScreen from '../screens/WelcomeScreen';
+import WelcomeScreen from '../../inSync/node_modules/WelcomeScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import LoginScreen from '../screens/LoginScreen';
 
